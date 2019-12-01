218149b7ea2392a13145ea6edad651e949545c3d
binymarcus <48927315+binymarcus@users.noreply.github.com>
2019-12-01 13:26:06 +0200

    i got till step six in the project

---
+++ dotNet5780_03_9916_08225/dotNet5780_03_9916_08225/Host.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dotNet5780_03_9916_08225
+{
+    public class Host
+    {
+        string HostName;
+        List<HostingUnit> Units;
+    }
+}
---
+++ dotNet5780_03_9916_08225/dotNet5780_03_9916_08225/HostingUnit.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dotNet5780_03_9916_08225
+{
+    public class HostingUnit
+    {
+        //we need to decide whats private and whats public,m although the class is public...    
+        string UnitName;
+        int Rooms;
+        bool IsSwimmingPool;
+        public List<DateTime> AllOrders { get; set; }// i think we need to actually do the get and set, unclear
+        public List<string> Uris { get; set; }// same thing with this one
+
+    }
+}
